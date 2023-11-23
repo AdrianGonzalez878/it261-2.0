b9954a78f462bfc015c9eb94f684a91bc84ad355
Adrian Gonzalez <139197746+AdrianGonzalez878@users.noreply.github.com>
2023-11-23 17:42:15 -0600

    Add files via upload

---
+++ website/thx.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank You!</title>
+    <link rel="stylesheet" href="css folder/styles.css">
+</head>
+
+<body>
+    <h1 class="thx">Thank You!</h1>
+    <p>Thank you for submitting the form. We appreciate your response.</p>
+</body>
+
+</html>
