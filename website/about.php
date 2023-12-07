e1e4550bc7996e3e5d46ee1ca689cdc6dba77289
Adrian Gonzalez <139197746+AdrianGonzalez878@users.noreply.github.com>
2023-12-06 23:33:04 -0600

    Add files via upload

---
+++ website/about.php
@@ -0,0 +1,10 @@
+<?php
+include('config.php');
+include('./includes/header.php');
+?>
+
+<h1 class="data">My DataBase!</h1>
+<img src="images folder/Mydatabase.png" alt="Mydatabase" class="border">
+
+<?php
+include('./includes/footer.php');
