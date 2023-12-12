3e1a74e516c28af1110e39253b506c9922247c5f
Adrian Gonzalez <139197746+AdrianGonzalez878@users.noreply.github.com>
2023-12-12 02:11:29 -0600

    Add files via upload

---
+++ weeks/week9/credentials.php
@@ -0,0 +1,6 @@
+<?php //my credentials that will allow me to connect to the database!!!
+
+define('DB_NAME', 'mystud13_fall2023');
+define('DB_USER', 'mystud13_fall2023_user'); 
+define('DB_PASSWORD', 'Conchito123');
+define('DB_HOST', 'localhost');
